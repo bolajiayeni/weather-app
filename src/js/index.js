7cb8b9a75940a38985360f45cc2129cda1945d50
bolajiayeni <ayenibolaji3@gmail.com>
2020-10-28 21:25:23 +0100

    Implemented MVC architecture and changed API

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
-import x from './test';
-const num = 25;
-console.log(x);
-console.log(num);
+import axios from 'axios';
+
+async function getWeather(query) {
+
+    const key = `fe68077f0519486daf1140943202810`;
+    const result = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=london&days=7`);
+
+    const data = await result.json();
+    console.log(data);
+}
+
+getWeather();
