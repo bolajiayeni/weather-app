eeab5dea9a01f85a0a494ef8c3e6ff8b6da4e860
bolajiayeni <ayenibolaji3@gmail.com>
2020-10-29 12:16:42 +0100

    built the search controller
    Next stop: Search view 🤪

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,36 @@
 import Search from './models/Search';
 
-const search = new Search('london');
-console.log(search);
-search.getResults();
+/**
+ * Global state variable
+ * contains:
+ * search object
+ * current location object
+ */
+const state = {};
+
+const controlSearch = async () => {
+
+    // 1. Get search query from the input field
+    const query = 'london'; //change this to receive query from search field
+
+    if (query) {
+
+        //2. create a new Search object and add it to the global state variable
+        state.search = new Search(query);
+
+        //3. prepare UI for search results i.e show spinner and clear previous searches etc
+
+        //4. send the query to the API
+        await state.search.getResults(); 
+
+        //5. Render the results to the UI
+        console.log(state.search.result);
+
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,13 +8,13 @@ export default class Search {
     async getResults () {
         try {
             const key = `fe68077f0519486daf1140943202810`;
-            const response = await axios(`http://api.weatherapi.com/v1/search.json?key=${key}&q=${this.query}&days=7`);
+            const response = await axios(`http://api.weatherapi.com/v1/search.json?key=${key}&q=${this.query}`);
 
             this.result = response.data;
-        } 
+        } //This function sends the search query to the weather api 
         
         catch (error) {
-            alert(`There seems to be a problem with the API, please contact the developer on twitter: @Bojthedev`);
+            alert(`There seems to be a problem with connecting to the API, please contact the developer on twitter: @Bojthedev`);
         }
     }
 } 
