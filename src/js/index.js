4847b363c13430be727c96921f402b3eff1875fa
bolajiayeni <ayenibolaji3@gmail.com>
2020-10-29 04:35:51 +0100

    bulit the search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getWeather(query) {
-
-    const key = `fe68077f0519486daf1140943202810`;
-    const result = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=london&days=7`);
-
-    const data = await result.json();
-    console.log(data);
-}
-
-getWeather();
+const search = new Search('london');
+console.log(search);
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
-export default 'I am a bitch';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query
+    }
+ 
+    async getResults () {
+        try {
+            const key = `fe68077f0519486daf1140943202810`;
+            const response = await axios(`http://api.weatherapi.com/v1/search.json?key=${key}&q=${this.query}&days=7`);
+
+            this.result = response.data;
+        } 
+        
+        catch (error) {
+            alert(`There seems to be a problem with the API, please contact the developer on twitter: @Bojthedev`);
+        }
+    }
+} 
